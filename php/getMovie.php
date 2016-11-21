da67c76fcfb8f87b2827d8db5b85e9502a1f49a8
Jaxon Wright <jaxonhwright@gmail.com>
2016-11-21 19:53:36 +0000

    began querying API for populateMovies.php, finished getMovie.php and getMovies.php, began populating front end content with dummy data

---
+++ php/getMovie.php
@@ -0,0 +1,36 @@
+<?php
+//mysql -p -h cis.gvsu.edu -u teitsmch
+// $servername = "cis.gvsu.edu";
+// $username = "teitsmch";
+// $password = "teitsmch0925";
+// $dbname = "teitsmch";
+    
+$servername = getenv('IP');
+$username = getenv('C9_USER');
+$password = "";
+$dbname = "c9";
+//$dbport = 3306;
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//header("Access-Control-Allow-Origin: *");
+if (!$conn){
+	die('Could not connect: '. mysql_error());
+}
+
+$MovieID =  $_GET['MovieID'];
+
+$sql="SELECT * FROM Movies WHERE MovieID = $MovieID";
+
+$resultSet = mysqli_query($conn, $sql);
+if (!$resultSet) { // add this check.
+    die('Invalid query: ' . mysql_error());
+}
+//appeand each record to our result array
+$retVal=array();
+$retVal[]=mysqli_fetch_assoc($resultSet);
+
+//encode the array as JSON
+echo json_encode($retVal);
+?>
--- php/populateMovies.php
+++ php/populateMovies.php
@@ -1,12 +1,38 @@
 <?php
+$servername = getenv('IP');
+$username = getenv('C9_USER');
+$password = "";
+$dbname = "c9";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 $key = 'fb8c9a3a6abeffc783e765d89f824ab9';
 $api = file_get_contents('http://api.themoviedb.org/3/movie/now_playing?api_key='.$key.'');
 $get = json_decode($api,true);
 
+
+$id = 0;
 foreach ($get['results'] as $a)
 {
-    echo var_dump($a);
     echo '<br>';
-    echo '<li><a href="/'.$a['original_title'].'" title="'.$a['original_title'].'"><small>'.$a['original_title'].'</small></a></li>';
+  //  echo '<li><a href="/'.$a['original_title'].'" title="'.$a['original_title'].'"><small>'.$a['original_title'].'</small></a></li>';
+    
+    //$sql = "INSERT INTO Movies
+    //VALUES(" . $id . "," . $a['original_title'] . "," . 'Hello' . "," . 'http://image.tmdb.org/t/p/w185/' . $a['poster_path'] . ")";
+    $sql = "INSERT INTO Movies
+    VALUES(3, 'title', 'description', 'url')";
+    echo $sql;
+//    echo var_dump($a);
+    if (mysqli_query($conn, $sql)) {
+        echo "Inserted row to table<br>";
+    } else {
+        echo "Error inserting row to table: " . mysqli_error($conn). "<br>";
+    }
+
+    
 }
 ?>
