87d85eb1a392906017ac8b03257422fdf146cff6
Jaxon Wright <jaxonhwright@gmail.com>
2016-11-22 05:05:43 +0000

    add IMDb ratings

--- movie.php
+++ movie.php
@@ -6,7 +6,7 @@
         foreach($array as $item) {
             echo "<div class='row'>
         			<div class='col-md-3'>
-        		        <div class='well well-sm' style='margin: 8px 8px 8px 8px; padding: 0px 0px 0px 0px'>";
+        		        <div class='well well-sm' style='margin: 8px 65px 8px 8px; padding: 0px 0px 0px 0px'>";
         	echo           "<img id='poster' alt='poster' src='".$item['PosterURL']."' width='190px' height='274px'/>
         	            </div>
         			</div>
@@ -17,17 +17,27 @@
         	echo	    "</p>
         			</div>
         			<div class='col-md-2'>
-        			   <div class='panel panel-warning'>
+        			   <div class='panel panel-default'>
         			      <div class='panel-heading'>
         					 <h3 class='panel-title'>IMDb Rating</h3>
         				   </div>
-        				   <div class='panel-body' id='imdbRating'></div>
+        				   <div class='panel-body' id='imdbRating'>";
+        				    include_once('./php/imdb.class.php');
+        		            $oIMDB = new IMDB($item['Name']);
+                            if ($oIMDB->isReady) {
+                                echo "<center><p class='lead'><strong>".$oIMDB->getRating()."</strong>/10</p></center>";
+                            } else {
+                                echo "<h1><strong>N/A</strong></h1>";
+                            }
+        	echo		  "</div>
         			   </div>
-        			   <div class='panel panel-danger'>
+        			   <div class='panel panel-default'>
         			      <div class='panel-heading'>
         					<h3 class='panel-title'>Rotton Tomatos</h3>
         			      </div>
-        			      <div class='panel-body' id='imdbRating'></div>
+        			      <div class='panel-body' id='rtRating'>";
+        		            
+        	echo         "</div>
         			   </div>
         			   <div class='panel panel-primary'>
         			      <div class='panel-heading'>
