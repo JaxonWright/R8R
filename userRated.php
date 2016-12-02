9d8fd38f2bb617fb777a0a0feaf375c9b8302e9c
Jaxon Wright <jaxonhwright@gmail.com>
2016-12-02 18:46:12 +0000

    restyled rating overlay

--- userRated.php
+++ userRated.php
@@ -10,8 +10,9 @@
             //echo "<div class='well well-sm col-sm-2' style='margin: 8px 8px 8px 8px; padding: 0px 0px 0px 0px'>";
             echo "
                         <a class='posterWithRating' href='#movie?id=".$item['MovieID']."'>
-                            <img class='poster' src='".$item['PosterURL']."' width='190px' height='274px'></img>
-                            <p class='overlay'>".$item['Rating']."</p>
+                            <img class='poster' src='".$item['PosterURL']."'>
+                                <h3 class='overlay'><strong>".$item['Rating']."★</strong></h3>
+                            </img>
                         </a>";
                         
             //echo "</div>";
